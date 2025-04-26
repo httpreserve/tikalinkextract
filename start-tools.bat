f3abc338d09edd67ee8a724af40b14b799145d48
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2025-04-26 21:15:53 +0200

    Fixup start scripts
    NB. `-e` is likely to be deprecated but I couldn't determine the correct
    use of the new syntax so have left it as-is today.

--- start-tools.bat
+++ start-tools.bat
@@ -4,6 +4,6 @@
 SET instDir="%~dp0\"
 
 :: Location of Tika server jar
-SET tikaServerJar=%instDir%\tools\tika-server-1.16.jar
+SET tikaServerJar=%instDir%\tools\tika-server-standard-3.1.0.jar
 
-start java -mx1000m -jar %tikaServerJar% --port=9998
+start java -mx1000m -jar %tikaServerJar% --port=9998 --noFork
--- start-tools.sh
+++ start-tools.sh
@@ -4,6 +4,6 @@
 instDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Location of Tika server jar
-tikaServerJar="$instDir"/tools/tika-server-1.16.jar
+tikaServerJar="$instDir"/tools/tika-server-standard-3.1.0.jar
 
-gnome-terminal -e "java -mx1000m -jar $tikaServerJar --port=9998"  #> /dev/null 2>&1 &
+gnome-terminal -e "java -mx1000m -jar $tikaServerJar --port=9998 --noFork"
